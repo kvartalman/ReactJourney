df7d70a011a3387faf0e98e3ef619eb9e5d645a0
kvartalman <ilyakov@yandex.ru>
2023-05-11 18:23:28 +0400

    Studying

---
+++ src/Technologies.js
@@ -0,0 +1,14 @@
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>How</li>
+                <li>Are</li>
+                <li>You</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Technologies;
+
