677183e27ad3ed0a3c38a2b3a0050b9e012fa80b
kvartalman <ilyakov@yandex.ru>
2023-05-14 13:16:29 +0400

    Keep working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,43 +7,36 @@ import {Button, Form} from "react-bootstrap";
 
 const Navigation = () => {
     return (
-        <Navbar bg="dark" expand="lg">
+        <Navbar bg="dark" expand="lg" id={'navigation-bar'}>
             <Container fluid>
                 <Navbar.Brand><img className={'logo'} src={'https://upload.wikimedia.org/wikipedia/ru/0/02/' +
                     'Heroes_of_the_Storm_BlizzHeroes_2017_logo.png?20230127151006'}
                                    alt={'Heroes of the Storm'}/></Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" id={'scroll-button'} />
+                <Navbar.Toggle aria-controls="navbarScroll" id={'scroll-button'}/>
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
                         className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
+                        style={{maxHeight: '100px'}}
                         navbarScroll
                     >
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
-                        <NavDropdown title="Link" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">
-                                Another action
+                        <Nav.Link href="#action1" id={'navbar-link'}>Main</Nav.Link>
+                        <Nav.Link href="#action2" id={'navbar-link'}>Boosters</Nav.Link>
+                        <NavDropdown title="Categories" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action3" id={'navbar-dropdown'}>Heroes of the
+                                Storm</NavDropdown.Item>
+                            <NavDropdown.Item href="#action4" id={'navbar-dropdown'}>
+                                Dota 2
                             </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">
-                                Something else here
+                            <NavDropdown.Item href="#action5" id={'navbar-dropdown'}>
+                                League of Legends
                             </NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="#" disabled>
-                            Link
+                        <Nav.Link href="#action6" id={'navbar-link'}>
+                            Help
                         </Nav.Link>
                     </Nav>
-                    <Form className="d-flex">
-                        <Form.Control
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="outline-success" id={'search-button'}>Search</Button>
-                    </Form>
+                    <Button variant="outline-success" id={'login-button'}>Login</Button>
+
                 </Navbar.Collapse>
             </Container>
         </Navbar>
