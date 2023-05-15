147e193dfcebee2081331b63c5c49d031c45b0f8
kvartalman <ilyakov@yandex.ru>
2023-05-15 17:25:22 +0400

    Fixed some mistakes with Row class, added 'advantage-cards' block

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Carousel from "./components/Carousel";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Container, Row} from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import Cards from "./components/Cards";
 import Advantages from "./components/Advantages";
 
