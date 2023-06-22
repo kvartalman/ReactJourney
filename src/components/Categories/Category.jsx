4f93ee976b3ca8c16170aed0eef7fbffbd3e31e8
Serega_Programmer <132437230+kvartalman@users.noreply.github.com>
2023-06-22 15:46:31 +0300

    Fixed bug that caused 'The play() request was interrupted by a call to pause()'

--- src/components/Categories/Category.jsx
+++ src/components/Categories/Category.jsx
@@ -1,39 +1,53 @@
-import React, {useRef, useState} from "react";
+import React, {useCallback, useRef, useState} from "react";
 import {NavLink} from "react-router-dom";
 
 const Category = ({bg, name, to, video}) => {
 
     const videoRef = useRef(null);
     const [show, setShow] = useState(false);
+    const [videoLoaded, setVideoLoaded] = useState(false);
+    const [videoPlaying, setVideoPlaying] = useState(false);
 
-    const toggleVideo = () => {
-        setShow(!show);
-        if (!show) {
+
+    // 2 functions that responsible for turning the videos on and off inside NavLinks
+    // Better not to touch because was problems with async pause() and play() before, now all works fine
+    const handleMouseEnter = useCallback(() => {
+        setShow(true);
+        if (videoRef.current.readyState >= 2) {
             videoRef.current.currentTime = 0;
-            if (videoRef.current.readyState >= 2) {
             videoRef.current.play();
-            } else {
-                videoRef.current.addEventListener('canplaythrough', function canPlayThrough() {
-                    videoRef.current.play();
-                    videoRef.current.removeEventListener('canplaythrough', canPlayThrough)
-                })
-            }
+            setVideoPlaying(true);
         } else {
-            videoRef.current.pause();
+            videoRef.current.addEventListener('canplaythrough', function canPlayThrough() {
+                setVideoLoaded(true);
+                videoRef.current.currentTime = 0;
+                videoRef.current.play();
+                setVideoPlaying(true);
+                videoRef.current.removeEventListener('canplaythrough', canPlayThrough)
+            })
         }
-    }
+    }, []);
+
+    const handleMouseLeave = useCallback(() => {
+        if (show && videoPlaying && videoLoaded) {
+            videoRef.current.pause();
+            setVideoPlaying(false);
+            setVideoLoaded(false);
+        }
+        setShow(false);
+    }, [show, videoPlaying, videoLoaded]);
 
     return (
         <span
             style={{background: `url(${bg})`}}
             className={'ctgGame'}
-            onMouseEnter={toggleVideo}
-            onMouseLeave={toggleVideo}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
         >
                 <NavLink className={'ctgLink'} to={to}>
                     {name}
                     <video ref={videoRef} className={'ctgVideo'} loop autoPlay muted>
-                        <source src={video} type='video/mp4' />
+                        <source src={video} type='video/mp4'/>
                     </video>
                 </NavLink>
             </span>
