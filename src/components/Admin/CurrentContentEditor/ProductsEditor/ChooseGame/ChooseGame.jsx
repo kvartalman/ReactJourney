ad299486854962b06da9a72c7bf54a5abf7ec960
Serega_Programmer <132437230+kvartalman@users.noreply.github.com>
2023-10-08 22:29:24 +0300

    Added sort for products

--- src/components/Admin/CurrentContentEditor/ProductsEditor/ChooseGame/ChooseGame.jsx
+++ src/components/Admin/CurrentContentEditor/ProductsEditor/ChooseGame/ChooseGame.jsx
@@ -8,15 +8,28 @@ const ChooseGame = (props) => {
 
     const [activeGameButton, setActiveGameButton] = useState(0);
     const [activeProductButton, setActiveProductButton] = useState(0);
+    const [sort, setSort] = useState(false);
+    const [productsCopy, setProductsCopy] = useState([...Object.keys(props.gameSelector[props.game].products)]);
+    const [showSortButtons, setShowSortButtons] = useState(false);
+    const alphabetSort = () => {
+        setSort(!sort)
+        if (sort) {
+            setProductsCopy(productsCopy.sort())
+        } else {
+            setProductsCopy([...Object.keys(props.gameSelector[props.game].products)]);
+        }
+    }
+
+    const sortButtonsFunc = () => {
+        setShowSortButtons(!showSortButtons);
+    }
 
     const handleProductSelect = (product, index) => {
-
         setActiveProductButton(index);
         props.setProduct(product);
     }
 
     const handleGameSelect = (game, index) => {
-
         setActiveGameButton(index);
         setActiveProductButton(0);
         props.setGame(game);
@@ -33,27 +46,48 @@ const ChooseGame = (props) => {
         </Button>
     ))
 
-    const productsButtons = Object.keys(props.gameSelector[props.game].products).map((product, index) => (
-        <Button
-            key={index}
-            onClick={() => handleProductSelect(product, index)}
-            className={activeProductButton === index ? 'activeButton' : 'defaultButton'}
-        >
-            {props.gameSelector[props.game].products[product].header}
-        </Button>
+    // In this map function we get only products without key 'cards' inside. We have cards only in subcategories and
+    // game pages
+
+    const productsButtons = productsCopy.map((product, index) => (
+        !props.gameSelector[props.game].products[product].cards ?
+            <Button
+                key={index}
+                onClick={() => handleProductSelect(product, index)}
+                className={activeProductButton === index ? 'activeButton' : 'defaultButton'}
+            >
+                {props.gameSelector[props.game].products[product].header}
+            </Button>
+            :
+            null
     ))
 
     return (
         <Container fluid id={'chooseButtonsMainContainer'}>
-                <div className={'gamesButtonsContainer'}>
-                    {gamesButtons}
-                </div>
-                <div className={'productsButtonsContainer'}>
-                    {productsButtons}
-                </div>
+            <div className={'gamesButtonsContainer'}>
+                {gamesButtons}
+            </div>
+            <div>
+                <Button className={'nextPageButton'} onClick={() => sortButtonsFunc()}>{showSortButtons ?
+                    'Hide sort types'
+                    :
+                    'Show sort types'
+                }
+                </Button>
+            </div>
+            {showSortButtons ?
                 <div>
-                    <Button className={'nextPageButton'} onClick={() => props.setKey('title')}>Next</Button>
+                    <Button className={'nextPageButton'} onClick={() => alphabetSort()}>Alphabet sort</Button>
                 </div>
+                :
+                null
+            }
+            <div className={'productsButtonsContainer'}>
+                {productsButtons}
+            </div>
+            <div>
+                <Button className={'nextPageButton'} onClick={() => props.setKey('title')}>Next</Button>
+            </div>
         </Container>
     );
 }
