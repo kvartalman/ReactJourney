d4139764d52dd16fbb778ebaf78a2429a0bcc077
Serega_Programmer <132437230+kvartalman@users.noreply.github.com>
2024-01-17 11:31:13 +0300

    working on new game adding

---
+++ src/components/Admin/NewContentSettings/NewGame/NewGameSubCategories/NewGameSubCategories.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './NewGameSubCategories.css';
+
+const NewGameSubCategories = () => {
+    return (
+        <div id={'newGameSubCategoriesMainContainer'}>
+
+        </div>
+    )
+}
+
+export default NewGameSubCategories;
